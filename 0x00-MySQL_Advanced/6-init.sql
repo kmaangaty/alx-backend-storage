302433bceb5d4dffd4cf45efef5d4e2d85ff9d44
Kmangaty <kmaangaty@gmail.com>
2024-05-08 20:02:19 +0300

    setup

--- 0x00-MySQL_Advanced/6-init.sql
+++ 0x00-MySQL_Advanced/6-init.sql
@@ -44,30 +44,3 @@ INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project
 
 INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_c, 91);
 INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_py, 73);
-
-bob@dylan:~$
-bob@dylan:~$ cat 6-init.sql | mysql -uroot -p holberton
-Enter password:
-bob@dylan:~$
-bob@dylan:~$ cat 6-bonus.sql | mysql -uroot -p holberton
-Enter password:
-bob@dylan:~$
-bob@dylan:~$ cat 6-main.sql
-Enter password:
--- Show and add bonus correction
-SELECT * FROM projects;
-SELECT * FROM corrections;
-
-SELECT "--";
-
-CALL AddBonus((SELECT id FROM users WHERE name = "Jeanne"), "Python is cool", 100);
-
-CALL AddBonus((SELECT id FROM users WHERE name = "Jeanne"), "Bonus project", 100);
-CALL AddBonus((SELECT id FROM users WHERE name = "Bob"), "Bonus project", 10);
-
-CALL AddBonus((SELECT id FROM users WHERE name = "Jeanne"), "New bonus", 90);
-
-SELECT "--";
-
-SELECT * FROM projects;
-SELECT * FROM corrections;
