0f88e8c71efc37c790154ae7ad18d8ced2e4d0c4
Kmangaty <kmaangaty@gmail.com>
2024-05-15 18:04:53 +0300

    setup

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,56 @@
+#!/usr/bin/env python3
+"""
+Module for web caching and tracking.
+"""
 import requests
 import redis
+from functools import wraps
+
+# Redis connection
+redis_conn = redis.Redis()
 
 
+def cache_and_track_access(method):
+    """
+    Decorator for counting how many times a URL is accessed
+    and caching the response.
+    """
+    @wraps(method)
+    def wrapper(url):
+        """
+        Wrapper function for caching and tracking URL accesses.
+
+        Args:
+            url (str): The URL to fetch.
+
+        Returns:
+            str: The HTML content of the URL.
+        """
+        cached_key = "cached:" + url
+        cached_data = redis_conn.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+        count_key = "count:" + url
+        html_content = method(url)
+        redis_conn.incr(count_key)
+        redis_conn.set(cached_key, html_content)
+        redis_conn.expire(cached_key, 10)
+
+        return html_content
+
+    return wrapper
+
+
+@cache_and_track_access
 def get_page(url: str) -> str:
     """
-    Function to get page content and cache it with expiration
+    Function to fetch the HTML content of a URL.
 
-    Parameters:
-        url (str): The URL of the page to fetch
+    Args:
+        url (str): The URL to fetch.
 
     Returns:
-        str: The content of the fetched page
+        str: The HTML content of the URL.
     """
-    redis_conn = redis.Redis()
-    count_key = f"count:{url}"
-    page_key = f"page:{url}"
-    count = redis_conn.incr(count_key)
-    if count == 1:
-        page_content = requests.get(url).text
-        redis_conn.setex(page_key, 10, page_content)
-        return page_content
-    else:
-        return redis_conn.get(page_key).decode()
+    response = requests.get(url)
+    return response.text
